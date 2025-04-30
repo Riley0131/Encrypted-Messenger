0d49f71afb62a002ad5ca487eb5fd1ee88197acc
Riley0131 <roshea@uccs.edu>
2025-04-30 14:14:35 -0600

    ServerClientComments

--- Client-Server/client.py
+++ Client-Server/client.py
@@ -21,6 +21,7 @@ def send_request(request):
 
 
 if __name__ == '__main__':
+    #controls for the client with different functions such as send, register, and get
     parser = argparse.ArgumentParser(description='Simple client for encrypted messaging server')
     parser.add_argument('--action', choices=['register', 'send', 'get'], required=True,
                         help='Action to perform: register a user, send a message, or get messages')
@@ -36,6 +37,7 @@ if __name__ == '__main__':
         'password': args.password
     }
 
+    #send function, will collect a message, encrypt it, and send it to the server
     if args.action == 'send':
         if not args.to or not args.message:
             print('Error: --to and --message are required for send')
